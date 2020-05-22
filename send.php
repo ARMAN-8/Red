3ca4901732b18cf424ba88432194ee16fbf0a4c3
Your Name <you@abc.net>
2020-05-22 12:51:06 +0600

    send.php added

---
+++ send.php
@@ -0,0 +1,36 @@
+<?php
+
+if(isset($_POST['submit'])){
+$to = "info@epicblog.net";; // Здесь нужно написать e-mail, куда будут приходить письма
+$from = $_POST['email']; // this is the sender's Email address
+$first_name = $_POST['first_name'];
+$subject = "Форма отправки сообщений с сайта";
+$subject2 = "Copy of your form submission";
+$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+$headers = "From:" . $from;
+$headers2 = "From:" . $to;
+
+mail($to,$subject,$message,$headers);
+// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+
+}
+
+?>
+
+/*
+<?php if (isset($_POST["otprav"])) {
+$to = "12test16@mail.ru";
+$subject = "Письмо с сайта";
+$charset = "utf-8";
+$headerss ="Content-type: text/html; charset=$charset\r\n";
+$headerss.="MIME-Version: 1.0\r\n";
+$headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
+$msg = "Имя: ".$_POST["name"]."\n";$msg .= "Сообщение: ".$_POST["msg"]."\n";
+mail($to, $subject, $msg, $headerss);
+print "<script>alert(\"Сообщение успешно отправлено!\");window.location = window.location.href</script>";
+}
+*/
